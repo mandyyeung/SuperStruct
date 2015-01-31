c9a9fec265d337d4e50591a0df90f66024b36736
Mandy Yeung <mandycodes@gmail.com>
2015-01-31 00:23:01 -0500

    Set up Devise with admin rights

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
